803c3d442462958a6f2565c3c850782d3fb92ccd
Jahanzaib <jzmalik123@gmail.com>
2023-10-15 15:15:15 +0500

    Fixed variables

--- src/bleu_api/client.py
+++ src/bleu_api/client.py
@@ -3,14 +3,14 @@ import requests
 
 class BleuAPIClient:
 
-    BASE_URL = "https://api.example.com"
-    CLIENT_ID = "4mfq2291m54gtg4t50gh83bcaa"
-    CLIENT_SECRET = "5uo8j2pq1dfmsmirkoim6rmse3og78ee0el86eltd6oh96fi3ao"
+    BASE_URL = "https://sdk.faceki.com"
+    # CLIENT_ID = "4mfq2291m54gtg4t50gh83bcaa"
+    # CLIENT_SECRET = "5uo8j2pq1dfmsmirkoim6rmse3og78ee0el86eltd6oh96fi3ao"
 
-    def __init__(self):
+    def __init__(self, client_id, client_secret):
         self.base_url = BleuAPIClient.BASE_URL
-        self.client_id = BleuAPIClient.CLIENT_ID
-        self.client_secret = BleuAPIClient.CLIENT_SECRET
+        self.client_id = client_id
+        self.client_secret = client_secret
 
     def make_request(self, endpoint, method='GET', params=None, data=None, headers=None):
         url = f"{self.base_url}/{endpoint}"
